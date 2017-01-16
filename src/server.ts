48076e16d3f347f456738fa862689c4367df8053
Luzian Zagadinow <luzianz@outlook.com>
2017-01-16 12:06:59 -0500

    removed retry functionality. delegated wiring routes. using AppSettings abstraction instead of loading app settings directly.

--- src/server.ts
+++ src/server.ts
@@ -2,52 +2,27 @@
 
 import * as restify from 'restify'; // http://restify.com
 import { readFileAsync } from './lib/fs-async';
-import getClaims from './rest-endpoints/claims/GET';
 import { AsyncListener } from './lib/restify-async';
-import { isSettings } from './type-checkers';
 import { setTimeoutAsync } from './lib/other-async';
-
-const settingsFilePath = '../app-settings.json';
-const settings = require(settingsFilePath);
+import { configureEndpoints } from './rest-endpoints';
+import AppSettings from './AppSettings';
 
 async function main() {
-	if (!isSettings(settings)) {
-		const errMsg = `Not all required properties are present in ${settingsFilePath}. Refer to ISettingsSchema.d.ts`;
-		throw new Error(errMsg);
-	}
+	const appSettings = new AppSettings();
 
-	let port = settings.port;
-
-	const server = restify.createServer({ name: settings.serviceName });
+	const server = restify.createServer({ name: appSettings.serviceName });
 
 	process.once('SIGINT', () => {
 		server.close();
 		console.log('service stopped');
 	});
 
-	server.get(`/api/${settings.serviceName}/claims`, restify.authorizationParser(), getClaims);
+	configureEndpoints(appSettings.serviceName, server);
 
 	const listener = new AsyncListener(server);
 
-	let retries = 5;
-
-	do {
-		try {
-			await listener.listenAsync(port, settings.host);
-			console.log(`service started: ${server.name} listening at ${server.url}`);
-			break;
-		} catch (err) {
-			if (err.code == 'EADDRINUSE') {
-				console.log(`port {port} is in use`);
-				await setTimeoutAsync(200);
-				retries--;
-				port++;
-			} else {
-				console.error(err);
-				break;
-			}
-		}
-	} while (retries > 0);
+	await listener.listenAsync(appSettings.port, appSettings.host);
+	console.log(`service started: ${server.name} listening at ${server.url}`);
 }
 
 main().then(
